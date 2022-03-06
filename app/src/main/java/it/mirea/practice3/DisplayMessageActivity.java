8ea516d7de27a64ce8a7379ed4834d90818e8a9a
Mikhail Aivazyan <aivazyan.com@gmail.com>
2022-03-06 15:18:00 +0300

    Initial commit

---
+++ app/src/main/java/it/mirea/practice3/DisplayMessageActivity.java
@@ -0,0 +1,17 @@
+package it.mirea.practice3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
